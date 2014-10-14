c63a5e9eef815c949ea2900c1650b45df7d5ef76
David Wotherspoon <david@dwotherspoon.com>
2014-10-14 13:44:07 +0100

    Ooops..

--- core/i8080.c
+++ core/i8080.c
@@ -6,6 +6,8 @@ void instr_nop(I8080_State * s) {
 	s->pc++;
 }
 
+/* Instruction decode table */
+
 const Instruction decode[] = 
 	{
 		&instr_nop, &instr_nop, &instr_nop, &instr_nop,
@@ -75,16 +77,27 @@ const Instruction decode[] =
 	};
 
 I8080_State * init_8080() {
-	I8080_State * ret = malloc(sizeof(I8080_State));
+	I8080_State * ret = ret = malloc(sizeof(I8080_State));
 	ret->pc = 0x0000;
 	ret->flags = 0x02;
 	ret->sp = 0xFFFF;
 	return ret;
 }
 
+/* Run... */
+void run(I8080_State * s) {
+	uint8_t hlt = 0;
+	while (!hlt) {
+		/* Execute current opcode */
+		(*decode[s->mem[s->pc]])(s);
+		printf("PC value: %d\n\r", s->pc);
+	}	
+}
 
 int main(int argc, char * argv[]) {
-	printf("Decode: %lu instructions.\n\r", (sizeof(decode)/sizeof(Instruction)));
 	I8080_State * r = init_8080();
+	printf("Decode: %lu instructions.\n\r", (sizeof(decode)/sizeof(Instruction)));
 	printf("SP: 0x%x.\n\r", r->sp);
+	run(r);
+	return 0;
 }
--- core/i8080.h
+++ core/i8080.h
@@ -1,6 +1,6 @@
 #include "stdint.h"
 
-
+/* Data for 8080 */
 typedef struct {
 	/* Registers */
 	uint8_t regs[7];
@@ -13,7 +13,21 @@ typedef struct {
 	/* Memory */
 	uint8_t mem[0xFFFF];
 	/* IO Devices */
+	struct IODevice * devices[256];
 } I8080_State; 
+
+
+/* Typedefs for IO device functionality.
+	Consider if passing state is neccesary...
+ */
+typedef uint8_t (*IO_in)(I8080_State *);
+typedef void (*IO_out)(I8080_State *);
+
+/* Typedef fdr IO Devices, implement in and out methods. */
+typedef struct {
+	IO_in in;
+	IO_out out;
+} IODevice;
 
 /* Typedef for instructions */
 typedef void (*Instruction)(I8080_State *);
