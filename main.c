5fbb108d62cda37552ffb9a94b0d6fa271d1692a
David Wotherspoon <david@dwotherspoon.com>
2014-10-17 09:47:03 +0100

    Add file error message.

--- main.c
+++ main.c
@@ -5,11 +5,15 @@ int main(int argc, char * argv[]) {
 	FILE * fp;
 	I8080_State * r = init_8080();
 	/* Init stuff */
-	fp = fopen("test.bin", "rb");
-	printf("Read in %lu bytes from test.bin\n\r", fread(r->mem, sizeof(uint8_t), 65536, fp));
-	fclose(fp);
+	if (fp = fopen("test.bin", "rb")) {
+		printf("Read in %lu bytes from test.bin\n\r", fread(r->mem, sizeof(uint8_t), 65536, fp));
+		fclose(fp);
 
-	/* Go */
-	run_8080(r);
+		/* Go */
+		run_8080(r);
+	}
+	else {
+		puts("No file; test.bin. Exiting.");
+	}
 	return 0;
 }
