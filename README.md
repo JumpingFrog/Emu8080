620c065a80e53df462b2be11e09d3c49ebb1a863
JumpingFrog <davidwother@hotmail.co.uk>
2014-11-07 17:35:20 +0000

    Refactor jumps

---
+++ README.md
@@ -0,0 +1,9 @@
+8080 Emulator in C
+===
+Does not attempt any kind of cyclic emulation. Uses a pointer array for fast instruction decoding.
+
+Currently implemented:
+- Data ops
+- Stack ops
+- Others here and there
+
--- core/groups/jump_group.c
+++ core/groups/jump_group.c
@@ -1,9 +1,57 @@
 #include "../i8080.h"
 #include "jump_group.h"
 
+/* Macro for conditional jumps */
+#define JMPC(C, S) uint16_t addr; if (C) { addr = S->mem[++S->pc];\
+											addr |= S->mem[++S->pc] << 8;\
+											S->pc = addr; } else { S-> pc += 3; }
+
+/* Jump unconditional */
 void instr_jmp(I8080_State * s) {
-	uint16_t addr;
-	addr = s->mem[++s->pc];
-	addr |= s->mem[++s->pc] << 8;
-	s->pc = addr;
+	JMPC(1, s);
+}
+
+/* Jump on carry */
+void instr_jc(I8080_State * s) {
+	JMPC((s->flags & FLG_C), s);
+}
+
+/* Jump on no carry */
+void instr_jnc(I8080_State * s) {
+	JMPC(!(s->flags & FLG_C), s);
+}
+
+/* Jump on zero */
+void instr_jz(I8080_State * s) {
+	JMPC((s->flags & FLG_Z), s);
+}
+
+/* Jump on no zero */
+void instr_jnz(I8080_State * s) {
+	JMPC(!(s->flags & FLG_Z), s);
+}
+
+/* Jump on positive */
+void instr_jp(I8080_State *s) {
+	JMPC(
+}
+
+/* Jump on minus */
+void instr_jm(I8080_State * s) {
+
+}
+
+/* Jump on parity even */
+void instr_jpe(I8080_State * s) {
+
+}
+
+/* Jump on parity odd */
+void instr_jpo(I8080_State * s) {
+
+}
+
+/* H & L to PC */
+void instr_pchl(I8080_State * s) {
+
 }
